e6e9efe0237b9e2568c8601fce415dee2aa45150
Lucas Emanuel <lucas0emanuel14@gmail.com>
2026-08-29 23:47:47 -0300

    refactor: redundance problems

--- src/com/recompensa/Recompensa.java
+++ src/com/recompensa/Recompensa.java
@@ -5,26 +5,23 @@ import java.util.Objects;
 public final class Recompensa {
     private final String item;
     private int quantidade;
-    private TipoItem tipo;
-    private Raridade raridade;
+    private final TipoItem tipo;
+    private final Raridade raridade;
 
     // Construtor
     public Recompensa(String item, int quantidade, TipoItem tipo, Raridade raridade){
         this.item = Objects.requireNonNull(item, "Item é obrigatório.").trim();
         this.tipo = Objects.requireNonNull(tipo, "Tipo é obrigatótio.");
-        this.raridade = Objects.requireNonNull(raridade, "Raridade é obrigatótio.");
+        this.raridade = Objects.requireNonNull(raridade, "Raridade é obrigatório.");
 
         if(this.item.isEmpty()){
             throw new IllegalArgumentException("Item não pode ficar vazio.");
         }
-        if(this.quantidade < 0){
+        if(quantidade < 0){
             throw new IllegalArgumentException("Quantidade não ser um valor negativo.");
         }
         
         this.quantidade = quantidade;
-        this.tipo = tipo;
-        this.raridade = raridade;
-
     }
 
     // Getters
