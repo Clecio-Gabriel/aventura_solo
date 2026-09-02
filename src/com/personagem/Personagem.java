3f3245730c622d1367e2edf09b5c9b15cb904ab2
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-09-02 14:00:15 -0300

    feat: new method created for Personagem

--- src/com/personagem/Personagem.java
+++ src/com/personagem/Personagem.java
@@ -2,6 +2,7 @@ package com.personagem;
 
 import com.item.Item;
 import com.personagem.inventario.Inventario;
+import java.util.ArrayList;
 
 public abstract class Personagem{
 
@@ -21,6 +22,12 @@ public abstract class Personagem{
         inv.add_item(item);
         System.out.printf("%s got an item!%nItem: %s%n%n", this.name, item);
     }
+    public void starting_inventory(ArrayList <Item> items){
+        this.inv = new Inventario();
+        for (Item item : items)
+            inv.add_item(item);
+    }
+    // public abstract String atacar();
 
     // [ III ] OVERRIDE METHODS
     @Override
