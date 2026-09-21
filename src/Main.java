c0c3edd1698187b2399ae6aaf2d6d2de72f6e4c0
Clecio-Gabriel <cleciogabofelix@gmail.com>
2026-09-20 22:46:17 -0300

    feat!: ItemBuilder almost done. only need to adjust the exceptions

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,12 @@ public class Main{
         Item i1 = new Armadura("Armadura de Diamante", Raridade.LENDARIO, 1, 20);
         System.out.println(i1);
 
+        Item i2 = new ItemBuilder("Espada de Ferro", TipoItem.ARMAMENTO, Raridade.LENDARIO)
+                  .with_strength(50)
+                  .totalQuantity(1)
+                  .build();
+        System.out.println(i2);
+
     }
 
 }
--- src/com/item/ItemBuilder.java
+++ src/com/item/ItemBuilder.java
@@ -5,4 +5,48 @@ import com.item.types.*;
 
 public final class ItemBuilder{
 
+    private final String name;
+    private final TipoItem type;
+    private final Raridade rarity;
+    private int quantity;
+
+    private int defense;
+    private int heal;
+    private int strength;
+
+    public ItemBuilder(String name, TipoItem type, Raridade rarity){
+        this.name = name;
+        this.type = Objects.requireNonNull(type);
+        this.rarity = Objects.requireNonNull(rarity);
+    }
+
+    public ItemBuilder totalQuantity(int quantity){
+        this.quantity = Math.max(0, Math.min(quantity, 64));
+        return this;
+    }
+    public ItemBuilder with_heal(int heal){
+        this.heal = heal;
+        return this;
+    }
+    public ItemBuilder with_defense(int defense){
+        this.defense = defense;
+        return this;
+    }
+    public ItemBuilder with_strength(int strength){
+        this.strength = strength;
+        return this;
+    }
+    public Item build(){
+        switch (this.type){
+            case TipoItem.ARMADURA:
+                return new Armadura(name, rarity, quantity, defense);
+            case TipoItem.CONSUMIVEL:
+                return new Consumivel(name, rarity, quantity, heal);
+            case TipoItem.ARMAMENTO:
+                return new Armamento(name, rarity, quantity, strength);
+            default:
+                throw new IllegalArgumentException("Invalid type of Item.");
+        }
+    }
+
 }
